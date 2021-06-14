66624283d6cfee42db1762b5bb52c89edd30adb8
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-06-14 10:13:23 +0300

    Changed variable

--- HTML/js/scripts.js
+++ HTML/js/scripts.js
@@ -1,13 +1,13 @@
 function naming(){
 
-    var CC=parseInt(document.getElementById("main").value);
-    var YY=parseInt(document.getElementById("name").value);
-    var MM=parseInt(document.getElementById("Form").value);
-    var DD=parseInt(document.getElementById("akan").value);
+    let CC=parseInt(document.getElementById("main").value);
+    let YY=parseInt(document.getElementById("name").value);
+    let MM=parseInt(document.getElementById("Form").value);
+    let DD=parseInt(document.getElementById("akan").value);
   
-    var day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sartuday"];
-    var femaleNames=["Akosua", "Adwoa","Abenaa","Akua","Yaa"," Afua","Ama"];
-    var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    let day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sartuday"];
+    let femaleNames=["Akosua", "Adwoa","Abenaa","Akua","Yaa"," Afua","Ama"];
+    let maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
   
     if(CC.value =="" || YY.value =="" || MM.value =="" || DD.value ==""){
         alert("No blank values allowed");
